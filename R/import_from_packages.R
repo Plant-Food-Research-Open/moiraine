518381311ace030221b47359c4388db31e759aa1
oliviaAB <olivia.angelinbonnet@gmail.com>
2024-01-11 10:07:56 +1300

    Added import_from_packages

---
+++ R/import_from_packages.R
@@ -0,0 +1,14 @@
+#' @importClassesFrom Biobase eSet SnpSet ExpressionSet
+NULL
+
+#' @importClassesFrom MultiDataSet MultiDataSet
+NULL
+
+#' @import methods
+NULL
+
+#' @importFrom dplyr sym
+NULL
+
+#' @importFrom ggplot2 aes
+NULL
